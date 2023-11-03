8eb227278235139e55c30bbd7c0e53ab716a4f29
Hoàng Hệ <thauminhphong191@gmail.com>
2023-11-03 15:43:33 +0700

    fix lỗi nhỏ

--- SerVer/controllers/delivery.controllers.js
+++ SerVer/controllers/delivery.controllers.js
@@ -1,4 +1,4 @@
-const DeliveryModel = require('../models/delivery.models');
+const DeliveryModel = require('../models/deliver.models');
 
 
 exports.getAllDelivery = async (req, res) => {
@@ -10,6 +10,38 @@ exports.getAllDelivery = async (req, res) => {
   }
 };
 
+exports.updateDelivery = async (req, res) => {
+  try {
+    const deliveryId = req.params.id;
+    const { name, phone, location, email } = req.body;
+    const updatedDelivery = await DeliveryModel.findByIdAndUpdate(
+      deliveryId,
+      { name, phone, location, email },
+      { new: true }
+    );
+    if (updatedDelivery) {
+      res.json(updatedDelivery);
+    } else {
+      res.status(404).json({ error: 'Delivery không tồn tại' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Lỗi server' });
+  }
+};
+
+exports.deleteDelivery = async (req, res) => {
+  try {
+    const deliveryId = req.params.id;
+    const deletedDelivery = await DeliveryModel.findByIdAndDelete(deliveryId);
+    if (deletedDelivery) {
+      res.json({ message: 'Xóa thành công' });
+    } else {
+      res.status(404).json({ error: 'Không tồn tại' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Lỗi server' });
+  }
+};
 
 exports.createDelivery = async (req, res) => {
   try {
