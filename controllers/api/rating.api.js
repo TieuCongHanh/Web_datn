8a908cf2ec183a724a9cc7c9f3d4d4ce1d819565
ducvtph25202 <ducvtph25202@fpt.edu.vn>
2023-12-15 17:40:58 +0700

    1

--- controllers/api/rating.api.js
+++ controllers/api/rating.api.js
@@ -19,8 +19,8 @@ exports.add = async(req, res, next) => {
             objRating.user_name = req.body.user_name;
             objRating.id_product = req.body.id_product;
             objRating.product_name = req.body.product_name;
-            objRating.product_test = req.body.product_test;
-            // await objRating.save();
+            // objRating.product_test = req.body.product_test;
+            await objRating.save();
         } catch (err){
             console.log(err);
         }
